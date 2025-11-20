a86aa28fdc99c6cd61320eac72709a2a1e29d4df
Pierre-Luc Bacon <pierre-luc.bacon@mila.quebec>
2025-11-19 20:18:11 -0500

    Execute notebooks during publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Build with BASE_URL=/rlbook for subdirectory deployment
-BASE_URL=/rlbook uv run jupyter-book build --html
+BASE_URL=/rlbook uv run jupyter-book build --html --execute
 # Deploy to GitHub Pages
 ghp-import -n -p -f _build/html
